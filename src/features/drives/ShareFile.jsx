e0bf9cac48db734c28d002aeea78722eb92e1b44
vodoanhoanglong <vodoanhoanglong10a9@gmail.com>
2022-04-05 19:23:14 +0700

    Add get file share mutation

---
+++ src/features/drives/ShareFile.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReplyIcon from '@mui/icons-material/Reply';
+export default function ShareFile() {
+  return (
+    <div>
+      <ReplyIcon />
+    </div>
+  );
+}
